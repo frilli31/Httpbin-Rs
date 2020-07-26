c37b3adc0312cd4141976c2cf32ebfa534972755
luca <luca.all1996@gmail.com>
2020-07-26 14:14:22 +0200

    Fixed clippy warning

--- src/routes.rs
+++ src/routes.rs
@@ -7,27 +7,27 @@ pub fn add_routes(cfg: &mut web::ServiceConfig) {
         .service(
             web::resource("/get")
                 .route(web::get().to(return_info_request))
-                .route(web::to(|| HttpResponse::MethodNotAllowed()))
+                .route(web::to(HttpResponse::MethodNotAllowed))
         )
         .service(
             web::resource("/post")
                 .route(web::post().to(return_info_request))
-                .route(web::to(|| HttpResponse::MethodNotAllowed()))
+                .route(web::to(HttpResponse::MethodNotAllowed))
         )
         .service(
             web::resource("/put")
                 .route(web::put().to(return_info_request))
-                .route(web::to(|| HttpResponse::MethodNotAllowed()))
+                .route(web::to(HttpResponse::MethodNotAllowed))
         )
         .service(
             web::resource("/delete")
                 .route(web::delete().to(return_info_request))
-                .route(web::to(|| HttpResponse::MethodNotAllowed()))
+                .route(web::to(HttpResponse::MethodNotAllowed))
         )
         .service(
             web::resource("/patch")
                 .route(web::patch().to(return_info_request))
-                .route(web::to(|| HttpResponse::MethodNotAllowed()))
+                .route(web::to(HttpResponse::MethodNotAllowed))
         )
     ;
 }
